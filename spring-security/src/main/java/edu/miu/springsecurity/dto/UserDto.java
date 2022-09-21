1ae75e439df525e7412aa123bc44b7a3b7510001
Zedagem <zedshif123@gmail.com>
2022-09-20 21:52:32 -0500

    Role added on UserDto

--- spring-security/src/main/java/edu/miu/springsecurity/dto/UserDto.java
+++ spring-security/src/main/java/edu/miu/springsecurity/dto/UserDto.java
@@ -1,8 +1,11 @@
 package edu.miu.springsecurity.dto;
 
+import edu.miu.springsecurity.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -12,4 +15,5 @@ public class UserDto {
     private String email;
     private String password;
     private String firstname;
+    private List<Role> roles;
 }
